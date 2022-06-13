123ece792a532ca03ad1403ac0c59186ad37e4e1
Almog Manor <ManorAlmog@outlook.com>
2022-06-13 12:45:39 +0300

    Add documentation

--- threading101/creating_threads.py
+++ threading101/creating_threads.py
@@ -1,30 +1,42 @@
 """
 Author: Almog Manor
-
+A script made to demonstrate the basics of multithreading in python
 """
 from threading import Thread
 from time import time
 
 
-RUNNING_TIME = 5 # in seconds
+RUNNING_TIME = 5  # in seconds
 
 
 def say_hello():
+    """prints Hello! for RUNNING_TIME seconds
+    """
     end_time = time() + RUNNING_TIME
     while time() < end_time:
         print("Hello!")
 
 
 def say_shalom():
+    """prints Shalom! for RUNNING_TIME seconds
+    """
     end_time = time() + RUNNING_TIME
     while time() < end_time:
         print("Shalom!")
 
 
 def main():
+    """This is where the program starts
+    """
+    # here we create a thread object
+    # threads have to be created and then started
+    # the target parameter is a POINTER to the function to run
+    # don't forget: it is a POINTER, DO NOT add () after the function name
     thread1 = Thread(target=say_hello)
     thread2 = Thread(target=say_shalom)
 
+    # here we start the thread, the main function continues to run
+    # without waiting for the function in thread1 to finish
     thread1.start()
     thread2.start()
 
